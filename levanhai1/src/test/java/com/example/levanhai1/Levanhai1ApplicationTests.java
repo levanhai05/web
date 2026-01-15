0f8a3529754235220a1b911ac6a0bfc84c88155c
Le Van Hai <lenvanhai123@gmail.com>
2026-01-15 10:32:20 +0700

    Add levanhai1

---
+++ levanhai1/src/test/java/com/example/levanhai1/Levanhai1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.levanhai1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Levanhai1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
